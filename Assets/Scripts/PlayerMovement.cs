9915b432d82b0373533172cbef922b5468dfab69
Mergery-GameDev <mergerywanjirumukoma@gmail.com>
2024-09-13 16:05:12 +0300

    Driving Sim
    Added Player movement and some challenges

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float forwardSpeed= 5.0f;
+    public float forwardSpeed= 15.0f;
+    public float turnSpeed = 10.0f;
+    public GameObject mainCamera;
+    public GameObject driverCamera;
 
     // Start is called before the first frame update
     void Start()
@@ -15,8 +18,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
         //FORWARD MOTION 
-        transform.Translate(Vector3.forward * forwardSpeed * Time.deltaTime);
+        transform.Translate(Vector3.forward * forwardSpeed * Time.deltaTime * verticalInput);
+
+        // TURN MOVEMENT 
+        transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+        
+        }
 
 
 
